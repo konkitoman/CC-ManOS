da998e2039f66e7a50af4c0f1fa974e0756508be
Konkitoman <konkitoman@users.noreply.github.com>
2022-01-30 20:21:04 +0200

    event_loop baisic coroutine added

--- event_loop.lua
+++ event_loop.lua
@@ -3,16 +3,42 @@ local module = {
     ended = false,
     tick = nil,
     idle_time = 0.1,
+    coroutines = {},
     run = function (self)
         self.tick = os.startTimer(self.idle_time)
         while not self.ended do
             while true do
-                local type, o1, o2, o3, o4 = os.pullEvent()
+                local type, o1, o2, o3, o4, o5, o6, o7, o8, o9 = os.pullEvent()
                 if type == "timer" then
                     if o1 == self.tick then
                         self.tick = os.startTimer(self.idle_time)
                         type = "update"
                         o1 = nil
+                    else
+                        for i = 1, #self.coroutines do
+                            local coroutine = self.coroutines[i]
+                            local to_remove = false
+                            local index = 0
+                                if coroutine then
+                                if coroutine.timer == o1 then
+                                    index = i
+                                    to_remove = true
+                                    if coroutine.callback then
+                                        if coroutine.args then
+                                            coroutine.callback(unpack(coroutine.args))
+                                        else
+                                            coroutine.callback()
+                                        end
+                                        else
+                                        type = "coroutine"
+                                        o1 = coroutine
+                                    end
+                                end
+                                if to_remove then
+                                    table.remove(self.coroutines, index)
+                                end
+                            end
+                        end
                     end
                 end
                 local args = {}
@@ -28,6 +54,21 @@ local module = {
                 if o4 then
                     table.insert(args, o4)
                 end
+                if o5 then
+                    table.insert(args, o5)
+                end
+                if o6 then
+                    table.insert(args, o6)
+                end
+                if o7 then
+                    table.insert(args, o7)
+                end
+                if o8 then
+                    table.insert(args, o8)
+                end
+                if o9 then
+                    table.insert(args, o9)
+                end
                 local event = {
                     type = type,
                     args = args
@@ -39,6 +80,13 @@ local module = {
     end,
     set_callback = function(self, callback)
         self.callback = callback
+    end,
+    add_coroutine = function (self, name, callback, args, time)
+        if not time then
+            time = 0
+        end
+        local timer = os.startTimer(time)
+        table.insert(self.coroutines, {timer = timer, name = name, callback = callback, args = args})
     end
 }
 
