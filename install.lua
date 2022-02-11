b0ca6e1c3419b93be75e5fc44b2a7ac2b5d5afad
Konkitoman <konkitoman@users.noreply.github.com>
2022-02-11 06:00:13 +0200

    add header

--- install.lua
+++ install.lua
@@ -1,3 +1,12 @@
+--[[
+    Install
+    by Konkito Man
+    
+    for: Lua Cobalt [https://github.com/SquidDev/Cobalt], CC-Tweaked, CC-ManOS [https://github.com/konkitoman/CC-ManOS]
+    LICENSE: MIT
+    Version: 0.0.1
+]]
+
 if not http then
     print("No http module!")
     return
